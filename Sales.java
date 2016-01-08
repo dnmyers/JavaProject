df8eb5d97ca767089c7be1c78095395dc9fd72cd
dnmyers <daniel.neil.myers@gmail.com>
2016-01-07 19:55:34 -0600

    Create Sales.java
    Sales class for Sales project.  Contains main method

---
+++ Sales.java
@@ -0,0 +1,14 @@
+//Sales.java
+//Created by: Daniel Myers
+
+import javax.swing.*;
+
+public class Sales extends JFrame{
+	public static void main(String[] args) {
+		SalesGUI gui = new SalesGUI();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(800,450);
+		gui.setVisible(true);
+	}
+}
+		
